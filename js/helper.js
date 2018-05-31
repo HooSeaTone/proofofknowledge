7bea3929d0d379f1a4d30f01cb62ab60c8e5cb5c
Chris C <chris@ccwebdevelopment.com>
2018-05-31 11:21:50 -0400

    Updated export feature for helper function

--- js/helper.js
+++ js/helper.js
@@ -1,4 +1,4 @@
-export function convert2Ascii(hexCode) {
+function convert2Ascii(hexCode) {
   let hex = hexCode.toString();//force conversion
   let str = '';
   for (let i = 0; (i < hex.length && hex.substr(i, 2) !== '00'); i += 2) {
@@ -6,3 +6,5 @@ export function convert2Ascii(hexCode) {
   }
   return str.replace(/\0/g, '')
 }
+
+module.exports = convert2Ascii
