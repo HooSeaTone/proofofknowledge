ca3c08aaf2b2af61d3a13f43eb9e37003f9aaff9
Chris C <chris@ccwebdevelopment.com>
2018-05-31 11:10:06 -0400

    Able to assign admins to the contract

--- contracts/TeachersPet.sol
+++ contracts/TeachersPet.sol
@@ -22,19 +22,36 @@ contract TeachersPet is StandardToken {
 
   struct Student {
     bytes32 id;
-    bytes32[] milestone;
+    bytes32[] milestones;
     uint tokensReceived;
   }
 
   mapping(bytes32 => Milestone) milestones;
   mapping(address => Student) students;
+  mapping(bytes32 => bool) admins;
 
-  modifier onlyOwner(){
-    require(msg.sender == owner);
+  modifier onlyAdmin() {
+    require(admins[keccak256(msg.sender)] == true || msg.sender == owner);
     _;
   }
 
-  function createMilestone(bytes32 _title, uint _reward, bytes32 _tags) onlyOwner public {
+  function addAdministrator(address _newAdmin) onlyAdmin public {
+    require(_newAdmin > 0);
+    admins[keccak256(_newAdmin)] = true;
+  }
+
+  function removeAdministrator(address _oldAdmin) onlyAdmin public {
+    require(_oldAdmin > 0);
+    require(admins[keccak256(_oldAdmin)] == true);
+    delete admins[keccak256(_oldAdmin)];
+  }
+
+  function isAdministrator(address _user) public view returns(bool) {
+    require(_user > 0);
+    return (admins[keccak256(_user)] || _user == owner);
+  }
+
+  function createMilestone(bytes32 _title, uint _reward, bytes32 _tags) onlyAdmin public {
     require(_title > 0 && _tags > 0 && _reward >= 0);
     Milestone memory milestone = Milestone(_reward,_tags);
     milestones[_title] = milestone;
@@ -46,31 +63,31 @@ contract TeachersPet is StandardToken {
     return (_title, milestones[_title].reward, milestones[_title].tags);
   }
 
-  function deleteMilestone(bytes32 _title) onlyOwner public {
+  function deleteMilestone(bytes32 _title) onlyAdmin public {
     require(_title > 0);
     require(milestones[_title].tags > 0);
     delete milestones[_title];
   }
 
-  function giveMilestone(address _student, bytes32 _title) onlyOwner public {
+  function giveMilestone(address _student, bytes32 _title) onlyAdmin public {
     require(_student > 0);
     require(_title > 0);
     require(students[_student].id > 0);
     require(milestones[_title].reward > 0 || milestones[_title].tags > 0);
 
-    uint history = students[_student].milestone.length;
+    uint history = students[_student].milestones.length;
     bool duplicate = false;
 
     // Check for duplicate milestone for student
     for (uint index = 0; index < history; index++) {
-      if(_title == students[_student].milestone[index] ) {
+      if(_title == students[_student].milestones[index] ) {
         duplicate = true;
         index = history;
       }
     }
 
     require(duplicate == false);
-    students[_student].milestone.push(_title);
+    students[_student].milestones.push(_title);
     students[_student].tokensReceived += milestones[_title].reward;
     approve(_student, milestones[_title].reward);
     transfer(_student, milestones[_title].reward);
@@ -86,17 +103,14 @@ contract TeachersPet is StandardToken {
     students[_address] = student;
   }
 
-  function getStudentRecord(address _student) public view returns(bytes32, bytes32[], uint){
+  function getStudentRecord(address _student) onlyAdmin public view returns(bytes32, bytes32[], uint){
     require(_student > 0);
     require(students[_student].id > 0);
-    return (students[_student].id, students[_student].milestone, students[_student].tokensReceived);
+    return (students[_student].id, students[_student].milestones, students[_student].tokensReceived);
   }
 
-  function deleteStudent(address _student) onlyOwner public returns(bool) {
-    // Check for valid address
-    require(_student != 0);
-
-    // Check for existing student
+  function deleteStudent(address _student) onlyAdmin public returns(bool) {
+    require(_student > 0);
     require(students[_student].id > 0);
 
     delete students[_student];
